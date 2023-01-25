bd10ba4e3f9421e599a4026bbd390f03f4f27ef7
snevyazh <snevyazh@gmail.com>
2023-01-25 17:11:50 +0200

    added flask to train_infer

--- train_inference.py
+++ train_inference.py
@@ -15,23 +15,24 @@ from firebase_admin import credentials, firestore
 from flask import Flask
 from flask import request
 
-df = pd.read_csv('Fire Station Database.csv')
-df_values = df.iloc[:,8:].fillna(0).to_numpy()
-X = df_values
+def predict():
+    df = pd.read_csv('Fire Station Database.csv')
+    df_values = df.iloc[:,8:].fillna(0).to_numpy()
+    X = df_values
 
-mixture, post = common.init(X,4)
+    mixture, post = common.init(X,4)
 
-post, cost = em.estep(X, mixture)
+    post, cost = em.estep(X, mixture)
 
-mixture = em.mstep(X, post, mixture)
+    mixture = em.mstep(X, post, mixture)
 
-mixture, post, cost = em.run(X, mixture, post)
+    mixture, post, cost = em.run(X, mixture, post)
 
-X = em.fill_matrix(X, mixture)
+    X = em.fill_matrix(X, mixture)
 
-X = StandardScaler().fit_transform(X)
+    X = StandardScaler().fit_transform(X)
 
-kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
+    kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
 
 
 
@@ -39,50 +40,53 @@ kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
 
 ### inference part starts here
 
-labels = kmeans.labels_
-print(labels)
+    labels = kmeans.labels_
+    return labels
 
-df_result = pd.DataFrame()
-df_result['color'] = labels
-df_result.reset_index(inplace=True)
-df_result.rename(columns={'index': 'employeeId'}, inplace=True)
-df_result['employeeId'] = df_result['employeeId'] +1000
-df_result.to_json('result.json', orient='records')
+def write_json(labels):
+    df_result = pd.DataFrame()
+    df_result['color'] = labels
+    df_result.reset_index(inplace=True)
+    df_result.rename(columns={'index': 'employeeId'}, inplace=True)
+    df_result['employeeId'] = df_result['employeeId'] +1000
+    df_result.to_json('result.json', orient='records')
 
-### This is FireBase part
 
-cd = credentials.Certificate("tmapper-a0c5d-firebase-adminsdk-uqyci-457e89c20c.json")
-firebase_admin.initialize_app(cd)
+def write_firebase():
+    ## This is FireBase part
 
-ref = db.reference(path='/', url="https://tmapper-a0c5d-default-rtdb.europe-west1.firebasedatabase.app/")
-with open("result.json", "r") as file:
-	file_contents = json.load(file)
-ref.set(file_contents)
+    cd = credentials.Certificate("tmapper-a0c5d-firebase-adminsdk-uqyci-457e89c20c.json")
+    firebase_admin.initialize_app(cd)
 
+    ref = db.reference(path='/', url="https://tmapper-a0c5d-default-rtdb.europe-west1.firebasedatabase.app/")
+    with open("result.json", "r") as file:
+        file_contents = json.load(file)
+    ref.set(file_contents)
+
+    ## end of firebase part
 
 
 # set Flask model and parameters
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/predict_churn')
-# def predict_churn():
-#     years_in_contract = float(request.args.get('years_in_contract'))
-#     age = int(request.args.get('age'))
-#     num_inters = int(request.args.get('num_inters'))
-#     is_male = int(request.args.get('is_male'))
-#     late_on_payment = int(request.args.get('late_on_payment'))
-#
-#     a = {0: 'years_in_contract', 4: 'late_on_payment', 2: 'num_inters', 3: 'is_male',   1: 'age'}
-#     X_temp = pd.DataFrame([years_in_contract, age, num_inters, is_male, late_on_payment]).T.rename(columns=a)
-#     print(X_temp)
-#     y_temp = clf_infer.predict(X_temp)
-#     print(y_temp[0])
-#     return str(y_temp[0])
+@app.route('/service')
+def service():
+    pred = int(request.args.get('predict'))
+    json = int(request.args.get('json'))
+    firebase = int(request.args.get('firebase'))
+    if pred >0:
+        labels = predict()
+    if json >0:
+        write_json(labels)
+    # if firebase >0:
+    #     write_firebase()
 
+    return "OK"
 
 
 
-# if __name__ == '__main__':
-#     app.run(port=5000)  # for local
-#     # app.run(host='0.0.0.0', port=8080)  # for AWS
+
+if __name__ == '__main__':
+    # app.run(port=5000)  # for local
+    app.run(host='0.0.0.0', port=8080)  # for AWS
 
