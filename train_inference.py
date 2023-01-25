b1d12ba5727b74161e16d111e26ff20e6c00a1c5
snevyazh <snevyazh@gmail.com>
2023-01-25 19:58:19 +0200

    changed the output to firebase. Added different numbers of clusters

--- train_inference.py
+++ train_inference.py
@@ -16,52 +16,40 @@ from flask import Flask
 from flask import request
 
 def predict():
-    df = pd.read_csv('Fire Station Database.csv')
-    df_values = df.iloc[:,8:].fillna(0).to_numpy()
-    X = df_values
+    final_dict = dict()
+    for num in range(3, 7):
+        mixture, post = common.init(X,4)
+        post, cost = em.estep(X, mixture)
+        mixture = em.mstep(X, post, mixture)
+        mixture, post, cost = em.run(X, mixture, post)
+        X = em.fill_matrix(X, mixture)
+        X = StandardScaler().fit_transform(X)
+        kmeans = KMeans(n_clusters=num, random_state=0).fit(X)
+        labels = kmeans.labels_
+        result_dict = dict()
+        for ind, val in enumerate(labels):
+            result_dict.update({int(ind+1000): int(val)})
+        final_dict.update({"cluster_num_{}".format(num): result_dict})
+    return final_dict
 
-    mixture, post = common.init(X,4)
-
-    post, cost = em.estep(X, mixture)
-
-    mixture = em.mstep(X, post, mixture)
-
-    mixture, post, cost = em.run(X, mixture, post)
-
-    X = em.fill_matrix(X, mixture)
-
-    X = StandardScaler().fit_transform(X)
-
-    kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
-
-
-
-### training part end here
-
-### inference part starts here
-
-    labels = kmeans.labels_
-    return labels
-
-def write_json(labels):
-    df_result = pd.DataFrame()
-    df_result['color'] = labels
-    df_result.reset_index(inplace=True)
-    df_result.rename(columns={'index': 'employeeId'}, inplace=True)
-    df_result['employeeId'] = df_result['employeeId'] +1000
-    df_result.to_json('result.json', orient='records')
+#
+#
+# def write_json(labels):
+#     df_result = pd.DataFrame()
+#     df_result['color'] = labels
+#     df_result.reset_index(inplace=True)
+#     df_result.rename(columns={'index': 'employeeId'}, inplace=True)
+#     df_result['employeeId'] = df_result['employeeId'] +1000
+#     df_result.to_json('result.json', orient='records')
 
 
-def write_firebase():
-    ## This is FireBase part
+def write_firebase(final_dict):
 
     cd = credentials.Certificate("tmapper-a0c5d-firebase-adminsdk-uqyci-457e89c20c.json")
     firebase_admin.initialize_app(cd)
 
     ref = db.reference(path='/', url="https://tmapper-a0c5d-default-rtdb.europe-west1.firebasedatabase.app/")
-    with open("result.json", "r") as file:
-        file_contents = json.load(file)
-    ref.set(file_contents)
+    ref.set(final_dict)
 
     ## end of firebase part
 
@@ -72,18 +60,12 @@ app = Flask(__name__)
 @app.route('/service')
 def service():
     pred = int(request.args.get('predict'))
-    json = int(request.args.get('json'))
     firebase = int(request.args.get('firebase'))
     if pred >0:
-        labels = predict()
-    if json >0:
-        write_json(labels)
-    # if firebase >0:
-    #     write_firebase()
-
+        final_dict = predict()
+    if firebase >0:
+        write_firebase(final_dict)
     return "OK"
-
-
 
 
 if __name__ == '__main__':
